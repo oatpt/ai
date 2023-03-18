f90dd595360d0c925ad70d2eaf63adafdc713117
oatpt <75560254+oatpt@users.noreply.github.com>
2023-03-19 02:09:17 +0700

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import requests
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from app.models import Webhook
+import json
+# Create your views here.
+def hello(request):
+
+    if request.method == 'GET':
+        # รับค่าตำแหน่งปัจจุบันของผู้ใช้
+        url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
+        location = "13.7293834,100.7754963"
+        radius = "1500"
+        types = "restaurant"
+        keyword = "food"
+        api_key = "AIzaSyB7bq0fwc6swgJWX8Rt3Ka5cJYWfqmerGY"
+
+        # สร้าง URL พร้อมใส่ API key และ parameter อื่น ๆ
+        request_url = f"{url}location={location}&radius={radius}&types={types}&keyword={keyword}&key={api_key}"
+
+        # เรียกใช้งาน API ด้วย HTTP GET request
+        response = requests.get(request_url)
+
+        # แสดงผลลัพธ์
+        print(response.json())
+        return HttpResponse(response)
+    
+@csrf_exempt
+def webhook(request):
+    if request.method == "POST":
+        # อ่านข้อมูลจาก request body
+        data = json.loads(request.body)
+        # ทำการเพิ่มข้อมูลลงใน Database
+        #webhook = Webhook.objects.create(payload=data)
+        # ส่งข้อมูลกลับไปยัง Dialogflow โดยใช้ JSON
+        response_data = {
+            "fulfillmentMessages": [
+                {
+                    "text": {
+                        "text": [
+                            "Webhook received!"
+                        ]
+                    }
+                }
+            ]
+        }
+        return JsonResponse(response_data, safe=False)
+    else:
+        return JsonResponse({}, safe=False)
